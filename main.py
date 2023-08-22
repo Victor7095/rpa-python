a4697c3de939e6f00beb9d8cff9374a5ddd46d2f
Victor7095 <victoryan345@gmail.com>
2023-08-22 11:48:32 -0400

    feat: download images

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.remote.webelement import WebElement
 import re
+import urllib.request
 
 from datetime import datetime
 import locators
@@ -224,6 +225,23 @@ def get_news_raw_results(search_phrase):
     return results
 
 
+def download_pictures(results):
+    create_dir("output/images")
+    for (index, result) in enumerate(results):
+        picture_url = result[3]
+        if picture_url:
+            picture_filename = picture_url.split('/')[-1]
+            # remove query string from url
+            picture_filename = picture_filename.split('?')[0]
+            extension = picture_filename.split('.')[-1]
+
+            urllib.request.urlretrieve(
+                picture_url, f"output/images/{index}_{picture_filename}.{extension}")
+            result[3] = picture_filename
+
+    return results
+
+
 def raw_data_to_list_of_dictionary(raw_data):
     # Convert raw data to a list of dictionaries
     results = []
@@ -274,6 +292,7 @@ def main():
         filter_by_categories(inputs["categories"])
         apply_date_filter(inputs["minDate"])
         raw_results = get_news_raw_results(inputs["searchPhrase"])
+        raw_results = download_pictures(raw_results)
 
         results = raw_data_to_list_of_dictionary(raw_results)
         write_excel_worksheet("output/results.xlsx", "Fresh News", results)
